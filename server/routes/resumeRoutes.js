86aac197523d1ec0c98a99fc5ff44b496e0f7f16
Abhinav2005-hub <abhinavsaini995@gmail.com>
2026-05-17 23:32:39 +0530

    integrate Gemini AI resume analysis

--- server/routes/resumeRoutes.js
+++ server/routes/resumeRoutes.js
@@ -2,43 +2,66 @@ const express = require("express");
 const multer = require("multer");
 const pdfParse = require("pdf-parse");
 
+const analyzeResumeWithAI = require("../utils/gemini");
+
 const router = express.Router();
 
+// Store files in memory
 const storage = multer.memoryStorage();
 
 const upload = multer({
-    storage: storage,
+  storage: storage,
 });
 
+// Upload + Parse + Analyze
 router.post(
-    "/upload",
-    upload.single("resume"),
-    async (req, res) => {
-        try {
-            if (!req.file) {
-                return res.status(400).json({
-                    success: false,
-                    message: "No file uploaded",
-                });
-            }
+  "/upload",
+  upload.single("resume"),
+  async (req, res) => {
 
-            const data = await pdfParse(req.file.buffer);
+    try {
 
-            const extractedText = data.text;
+      if (!req.file) {
+        return res.status(400).json({
+          success: false,
+          message: "No file uploaded",
+        });
+      }
 
-            res.json({
-                success: true,
-                extractedText,
-            });
-        } catch (error) {
-            console.log(error);
+      // Parse PDF
+      const data = await pdfParse(req.file.buffer);
 
-            res.status(500).json({
-                success: false,
-                message: "Error parsing pdf",
-            });
-        }
+      const extractedText = data.text;
+
+      // AI Analysis
+      const aiResponse = await analyzeResumeWithAI(
+        extractedText
+      );
+
+      // Remove markdown formatting
+      const cleanedResponse = aiResponse
+        .replace(/```json/g, "")
+        .replace(/```/g, "")
+        .trim();
+
+      // Convert JSON string → JS object
+      const parsedResponse = JSON.parse(cleanedResponse);
+
+      res.json({
+        success: true,
+        analysis: parsedResponse,
+      });
+
+    } catch (error) {
+
+      console.log(error);
+
+      res.status(500).json({
+        success: false,
+        message: "Error analyzing resume",
+      });
     }
+  }
 );
 
 module.exports = router;
